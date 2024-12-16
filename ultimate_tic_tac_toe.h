519b629b00dabd6ec50948c4929c2b3cf27fdfce
mahmoudHP <157931767+mahmoudHP@users.noreply.github.com>
2024-12-16 06:26:15 -0800

    Update ultimate_tic_tac_toe.h

--- ultimate_tic_tac_toe.h
+++ ultimate_tic_tac_toe.h
@@ -4,13 +4,13 @@
 #include <iostream>
 using namespace std;
 
-// تعريف دالة ultimate_tic_tac_toe
+
 void ultimate_tic_tac_toe() {
     char meta_board[3][3] = { '\0' };
     char player = 'X';
 
     while (true) {
-        // عرض حالة اللوحة الرئيسية
+        
         cout << "\nMeta-Game Board Status:\n";
         for (int i = 0; i < 3; ++i) {
             for (int j = 0; j < 3; ++j) {
@@ -19,15 +19,14 @@ void ultimate_tic_tac_toe() {
             cout << endl;
         }
 
-        // إدخال مكان اللوحة الصغرى
+      
         int sb_row, sb_col;
         cout << "\n" << player << "'s turn. Select a small board to play (row col): ";
         cin >> sb_row >> sb_col;
 
-        // التحقق من صحة الإدخال
+       
         if (sb_row >= 0 && sb_row < 3 && sb_col >= 0 && sb_col < 3 && meta_board[sb_row][sb_col] == '\0') {
             meta_board[sb_row][sb_col] = player;
-            // تبديل اللاعب
             player = (player == 'X') ? 'O' : 'X';
         } else {
             cout << "Invalid choice. Try again.\n";
